3df17a7df3b5c4c91706bfce9ba7d851d6d17820
Tshiver <hassan.benadi99@gmail.Com>
2025-02-07 20:49:05 +0100

    add login functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 # import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-
+DBPATH="data/database.db"
 
 @app.route('/')
 def home():
@@ -19,6 +19,16 @@ def login():
 def create():
     return render_template('create.html')
 
+@app.route('/login_account',methods=['POST','GET'])
+def login_account():
+    username = request.form.get('username', None)
+    password = request.form.get('pass', None)
+    t=get_user(username,password)
+    if((username=='' or password=='')or len(t)==0):
+        return render_template('index.html',server_msg='invalid username or password')
+    else:
+        return render_template('index.html',server_msg='')
+
 
 @app.route('/create_account',methods=['POST','GET'])
 def create_account():
@@ -27,11 +37,11 @@ def create_account():
     if(username=='' or password==''):
         return render_template('index.html',server_msg='ya 7mar da5l bl s7i7')
     else:
-
+        insert_user(username,password)
         return render_template('index.html',server_msg='')
 
 def create_tables():
-    conn = sq.connect('database.db')
+    conn = sq.connect(DBPATH)
     cur = conn.cursor()
     cur.execute('''
                 CREATE TABLE IF NOT EXISTS users
@@ -41,13 +51,29 @@ def create_tables():
     conn.close()
 
 
-def insert_user():
-    conn = sq.connect('users.db')
+def get_user(user,password):
+    conn = sq.connect(DBPATH)
     cur = conn.cursor()
+    res = cur.execute("SELECT username FROM users WHERE username = ? AND password = ?",(user,password))
+    t=res.fetchall()
+    conn.commit()
+    conn.close()
+    return t 
+
+
+def insert_user(user,password):
+    conn = sq.connect(DBPATH)
+    cur = conn.cursor()
+    cur.execute('''
+                INSERT INTO users VALUES(?,?)
+                ''',(user,password,))
+    conn.commit()
+    conn.close()
 
 
 
 def main():
+    create_tables()
     app.run(debug=True)
 
 
